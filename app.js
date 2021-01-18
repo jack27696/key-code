51ec1fbceccdc39c3340402b32e8f930a6d7d960
Jack Edwards <jackedwards2706@gmail.com>
2021-01-18 15:11:23 +0000

    Added some more styling and some info boxes.

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 //get the keycode element
 const keycode_title = document.querySelector("#keycode h1");
+const eventkey = document.querySelector("#eventkey h2");
+const eventcode = document.querySelector("#eventcode h2");
 
 //Listern for keydown event
 document.addEventListener("keydown", (event) =>{
 
 	keycode_title.innerText = event.keyCode;
+	eventkey.innerText = event.key;
+	eventcode.innerText = event.code;
 
 });
